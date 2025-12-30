43f180463f785e74f01fecb7de219836c8c70331
Elena Lee <tbfjs3976@icloud.com>
2025-12-31 00:38:20 +0900

    12/30

--- src/collection/set/MyHashSetV2.java
+++ src/collection/set/MyHashSetV2.java
@@ -3,52 +3,52 @@ package collection.set;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-public class MyHashSetV1 {
+public class MyHashSetV2 {
 
     static final int DEFAULT_INITIAL_CAPACITY = 16;
 
-    LinkedList<Integer>[] buckets;
+    LinkedList<Object>[] buckets;
 
     private int size = 0;
     private int capacity = DEFAULT_INITIAL_CAPACITY;
 
-    public MyHashSetV1() {
+    public MyHashSetV2() {
         initBuckets();
     }
 
-    public MyHashSetV1(int capacity) {
+    public MyHashSetV2(int capacity) {
         this.capacity = capacity;
         initBuckets();
     }
 
-    private void initBuckets() {
+    private void initBuckets() { // 연결리스트 들어갈 배열 생성
         buckets = new LinkedList[capacity];
         for (int i = 0; i < capacity; i++) {
             buckets[i] = new LinkedList<>();
         }
     }
 
-    public boolean add(int value) {
-        int hashIndex = hashIndex(value);
-        LinkedList<Integer> bucket = buckets[hashIndex];
-        if (bucket.contains(value)) {
-            return false;
+    public boolean add(Object value) {
+        int hashIndex = hashIndex(value); // 해시 인덱스 구하기
+        LinkedList<Object> bucket = buckets[hashIndex]; // 해당 인덱스 배열 가져오기
+        if (bucket.contains(value)) { // 데이터 중복 검사
+            return false; // ⚠️ 반환되면 그냥 실행 종료임❗️
         }
         bucket.add(value);
         size++;
         return true;
     }
 
-    public boolean contains(int searchValue) {
+    public boolean contains(Object searchValue) { // set은 중복이 안돼요
         int hashIndex = hashIndex(searchValue);
-        LinkedList<Integer> bucket = buckets[hashIndex];
+        LinkedList<Object> bucket = buckets[hashIndex];
         return bucket.contains(searchValue);
     }
 
-    public boolean remove(int value) {
+    public boolean remove(Object value) {
         int hashIndex = hashIndex(value);
-        LinkedList<Integer> bucket = buckets[hashIndex];
-        boolean result = bucket.remove(Integer.valueOf(value));
+        LinkedList<Object> bucket = buckets[hashIndex];
+        boolean result = bucket.remove(value);
         if (result) {
             size--;
             return true;
@@ -57,20 +57,21 @@ public class MyHashSetV1 {
         }
     }
 
+    private int hashIndex(Object value) {
+        // hashCode 의 결과로 음수가 나올 수 있음.  abs() 사용해서 음수 제거
+        return Math.abs(value.hashCode()) % capacity;
+    }
+
     public int getSize() {
         return size;
     }
 
     @Override
     public String toString() {
-        return "MyHashSetV1{" +
+        return "MyHashSetV2{" +
                 "buckets=" + Arrays.toString(buckets) +
                 ", size=" + size +
                 ", capacity=" + capacity +
                 '}';
-    }
-
-    private int hashIndex(int value) {
-        return value % capacity;
     }
 }
